dddb9b62b1eb850a4ba89699033c1ab64b61b46e
Michał Czuba <michal.czuba.1995@gmail.com>
2021-08-16 18:44:41 +0200

    implemented second metric

--- src/rankings.py
+++ src/rankings.py
@@ -1,10 +1,8 @@
-import matplotlib.pyplot as plt
 import networkx as nx
-from typing import List, Tuple, Set, Dict
-from dataclasses import dataclass
+from typing import List, Tuple, Dict
 import logging
 
-from .tree import draw_tree, find_sub_trees
+from .tree import find_sub_trees
 
 log = logging.getLogger(__name__)
 
@@ -30,10 +28,11 @@ def compute_ranking(tree: nx.DiGraph, method: str) -> List[Tuple[str, int]]:
 
 
 def _compute_subtree_count(tree: nx.DiGraph) -> List[Tuple[str, int]]:
-    """Computes ranking of tree basing of count of subtrees with the same color."""
+    """Compute ranking of tree basing of count of subtrees with the same color."""
     metric = {}
-    for sub_tree in find_sub_trees(tree):
-        color = tree.nodes[sub_tree.pop()]['color']
+    for sub_tree_nodes in find_sub_trees(tree):
+
+        color = tree.nodes[sub_tree_nodes.pop()]['color']
         if metric.get(color) is None:
             metric[color] = 1
         else:
@@ -43,7 +42,20 @@ def _compute_subtree_count(tree: nx.DiGraph) -> List[Tuple[str, int]]:
 
 
 def _compute_subtree_max_depth(tree: nx.DiGraph) -> List[Tuple[str, int]]:
-    return 1
+    """Compute ranking of tree basing on a max depth among subtrees of the same color."""
+    metric = {}
+    for sub_tree_nodes in find_sub_trees(tree):
+
+        sub_tree = nx.subgraph(tree, sub_tree_nodes)
+        depth = max(nx.shortest_path_length(sub_tree, min(sub_tree_nodes)).values()) + 1
+
+        color = tree.nodes[sub_tree_nodes.pop()]['color']
+        if metric.get(color) is None:
+            metric[color] = depth
+        else:
+            metric[color] = max(depth, metric[color])
+
+    return prepare_result(metric)
 
 
 def _compute_subtree_average_value(tree: nx.DiGraph) -> List[Tuple[str, int]]:
